fc47cc41182cf185b80d4e6ccf95226b88bc5bf1
firstPush <adel.sdet2@gmail.com>
2025-06-16 21:25:00 -0500

    transforming data

--- src/CSVFileReader.ts
+++ src/CSVFileReader.ts
@@ -1,12 +1,11 @@
 import fs from 'fs';
-import { dateStringToDate } from './utils';
-import { MatchResult } from './MatchResult';
 
 
-type MatchData = [Date, string, string, number, number, MatchResult, string];
+
+
 
 export class CSVFileReader {
-  data: MatchData[] = [];
+  data: string[][] = [];
   
   constructor(public filename: string) {}
 
@@ -16,20 +15,6 @@ export class CSVFileReader {
       .map((row:string):string[] => {
         return row.split(',');
     
-      }).map((row: string[]): MatchData => {
-        return [
-          dateStringToDate(row[0]),
-          row[1],
-          row[2],
-          parseInt(row[3]),
-          parseInt(row[4]),
-          row[5] as MatchResult, //type assertion
-          row[6]
-
-        ];
-
-        
       });
-
   }
 }
