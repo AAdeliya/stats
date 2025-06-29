787f69027297cd5c57fe90d0a1878fcea2fd3796
firstPush <adel.sdet2@gmail.com>
2025-06-28 21:04:10 -0500

    a composition-based approach

---
+++ build/Summary.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Summary = void 0;
+class Summary {
+    constructor(analyzer, output) {
+        this.analyzer = analyzer;
+        this.output = output;
+    }
+}
+exports.Summary = Summary;
