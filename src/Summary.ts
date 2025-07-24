44c0cf56d68ae36f2e6421028477a9b00b7ed276
firstPush <adel.sdet2@gmail.com>
2025-07-24 12:33:52 -0500

    building the Reporter

--- src/Summary.ts
+++ src/Summary.ts
@@ -11,8 +11,12 @@ export interface OutputTarget {
 }
 
 export class Summary {
-  constructor(public analyzer: Analyzer, public output: OutputTarget) {
+  constructor(public analyzer: Analyzer, public outputTarget: OutputTarget) {
 
+    buildAndPrintReport(matches: MatchData[]): void {
+      const output = this.analyzer.run(matches);
+      this.outputTarget.print(output);
+    }
   } 
 }
 
